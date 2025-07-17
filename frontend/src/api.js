3b0fbe5f4a4f43f6826218897e7c9e637eacd14d
KABIR MATHUR <kabirmathur@KABIRs-MacBook-Air.local>
2025-07-17 18:58:58 +0530

    Final commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const isProd = process.env.NODE_ENV === 'production';
+const api = axios.create({
+  baseURL: isProd ? 'https://habitwise.onrender.com/api' : '/api'
+});
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
