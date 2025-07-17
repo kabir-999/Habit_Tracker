a6616cf61c42505d5c8d1e6b06983fe4bc6474c2
KABIR MATHUR <kabirmathur@KABIRs-MacBook-Air.local>
2025-07-17 19:00:52 +0530

    Final commit

--- frontend/api/models/Badge.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const BadgeSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  emoji: { type: String, required: true }
-});
-
-module.exports = mongoose.model('Badge', BadgeSchema); 
